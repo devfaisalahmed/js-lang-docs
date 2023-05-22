e7b7e76a39df3d849a098fd3ab0d47c32eea49e7
faisal <faisalbiswash61@gmail.com>
2023-05-22 22:32:13 +0600

    array

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
 var numArray = [10, 15, 20, 25, 30, 35, 40, 50, 55];
-// var result = numArray.filter(function (item) {
-//   return item < 25;
-// });
-// document.write(result);
-// numArray.forEach(function (item) {
-//   document.write(item + "<br/>");
-// });
-var n = numArray.indexOf(35);
-document.write(n);  
+var sl = numArray.slice(3, 5);
+document.write(sl)
