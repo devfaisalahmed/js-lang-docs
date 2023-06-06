e75f17b2f683ef9645a135294c6278124e230aef
faisal <faisalbiswash61@gmail.com>
2023-06-06 15:13:16 +0600

    xml

--- README.md
+++ README.md
@@ -294,31 +294,38 @@ var platforms = navigator.platform();
  * function - open(), send(),setRequestHeader()
  */
 const makeRequest = (method, url, data) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.onload = () => {
-    let data = xhr.response;
-    console.log(JSON.parse(data));
-  };
-  xhr.onerror = () => {
-    console.log("error is here");
-  };
-  xhr.send(JSON.stringify(data));
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = () => {
+      let data = xhr.response;
+      console.log(JSON.parse(data));
+    };
+    xhr.onerror = () => {
+      console.log("error is here");
+    };
+    xhr.setRequestHeader( 'Content-type', 'application/json')
+    xhr.send(JSON.stringify(data));
+  });
 };
 
 const getData = () => {
-  makeRequest("GET", "https://jsonplaceholder.typicode.com/posts");
-};
-// getData();
+  makeRequest("GET", "https://jsonplaceholder.typicode.com/posts")
+  .then((res) => console.log(res));
+}
+getData();
+
 
 const sendData = () => {
-  makeRequest("POST", "https://jsonplaceholder.typicode.com/posts", {
-    title: "foo",
-    body: "bar",
+  makeRequest("POST", "https://jsonplaceholder.typicode.com/posts",{
+    title: 'foo',
+    body: 'bar',
     userId: 1,
-  });
-};
+  })
+  .then((res) => console.log(res));
+}
+sendData();
+
 //sendData();
 
 const updateData = () => {
--- app.js
+++ app.js
@@ -4,46 +4,34 @@
  * function - open(), send(),setRequestHeader()
  */
 const makeRequest = (method, url, data) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Content-type", "application/json");
-  xhr.onload = () => {
-    let data = xhr.response;
-    console.log(JSON.parse(data));
-  };
-  xhr.onerror = () => {
-    console.log("error is here");
-  };
-  xhr.send(JSON.stringify(data));
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = () => {
+      let data = xhr.response;
+      console.log(JSON.parse(data));
+    };
+    xhr.onerror = () => {
+      console.log("error is here");
+    };
+    xhr.setRequestHeader( 'Content-type', 'application/json')
+    xhr.send(JSON.stringify(data));
+  });
 };
 
 const getData = () => {
-  makeRequest("GET", "https://jsonplaceholder.typicode.com/posts");
-};
-// getData();
+  makeRequest("GET", "https://jsonplaceholder.typicode.com/posts")
+  .then((res) => console.log(res));
+}
+getData();
+
 
 const sendData = () => {
-  makeRequest("POST", "https://jsonplaceholder.typicode.com/posts", {
-    title: "foo",
-    body: "bar",
+  makeRequest("POST", "https://jsonplaceholder.typicode.com/posts",{
+    title: 'foo',
+    body: 'bar',
     userId: 1,
-  });
-};
-//sendData();
-
-const updateData = () => {
-  makeRequest("PUT", "https://jsonplaceholder.typicode.com/posts/1", {
-    id: 1,
-    title: "fooMa",
-    body: "barMa", 
-    userId: 1,
-  });
-};
-// updateData();
-
-const updateSingleData = () => {
-  makeRequest("PATCH", "https://jsonplaceholder.typicode.com/posts/1", {
-    title: "Title is changed",
-  });
-};
-updateSingleData();
+  })
+  .then((res) => console.log(res));
+}
+sendData();
